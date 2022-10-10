98e182629c629e057440d8415221e99ccff45a2f
christo.vahtramae <christo.vahtramae@khk.ee>
2022-10-10 09:08:53 +0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import "./Navbar.css";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+  const [display, setDisplay] = useState("none");
+
+  const toggleDropdown = () => {
+    if (display === "none") {
+      setDisplay("flex");
+    } else {
+      setDisplay("none");
+    }
+  };
+
+  return (
+    <header>
+      <nav className="navbar flex">
+        <button className="hamburger-toggle flex" onClick={toggleDropdown}>
+          <div className="hamburger-lines flex">
+            <span className="hamburger-line"></span>
+            <span className="hamburger-line"></span>
+            <span className="hamburger-line"></span>
+          </div>
+          <p className="hamburger-text">Menu</p>
+        </button>
+        <ul className="nav-links" style={{ display: display }}>
+          <li className="nav-link">
+            <a href="#">Home</a>
+          </li>
+          <li className="nav-link">
+            <a href="#">Blog</a>
+          </li>
+          <li className="nav-link">
+            <a href="#">Events</a>
+          </li>
+          <li className="nav-link">
+            <a href="#">Gallery</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
