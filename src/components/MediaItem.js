32794c3a247609d523b017e8148052c4e7916eb6
Christo-IV <christo.vahtramae@voco.ee>
2022-12-02 15:59:00 +0200

    Brought peace between "going" and "Tomorrow" they may now coexist

--- src/components/MediaItem.js
+++ src/components/MediaItem.js
@@ -11,29 +11,30 @@ const MediaItem = ({ name, type, imgUrl, keywords, date, going }) => {
     <div className="mediaItem flex">
       <div className={`mediaItem-img flex ${type}`}>
         <p className="mediaItem-date">{dayOfMonth[2] + " " + dayOfMonth[1]}</p>
-        {typeof imgUrl !== "undefined" && <img src={imgUrl} alt={name} />}
+        {imgUrl && <img src={imgUrl} alt={name} />}
       </div>
       <div className="mediaItem-content flex">
         <h3 className="mediaItem-name">{name}</h3>
         <ul className="mediaItem-keywords flex">
           {keywords.map((keyword, index) => (
-            <li className="mediaItem-keyword flex" key={name + index}>
+            <>
               {index % 2 === 1 && <span className="dividing-dot"></span>}
-              {keyword}
-            </li>
+              <li className="mediaItem-keyword flex" key={name + index}>
+                {keyword}
+              </li>
+            </>
           ))}
-          {going ? (
+          {going && (
             <>
               <span className="dividing-dot"></span>
               <li className="mediaItem-keyword success">Going</li>
             </>
-          ) : (
-            tomorrow.toDateString() === mediaItemDate.toDateString() && (
-              <>
-                <span className="dividing-dot"></span>
-                <li className="mediaItem-keyword danger">Tomorrow</li>
-              </>
-            )
+          )}
+          {tomorrow.toDateString() === mediaItemDate.toDateString() && (
+            <>
+              <span className="dividing-dot"></span>
+              <li className="mediaItem-keyword danger">Tomorrow</li>
+            </>
           )}
         </ul>
       </div>
