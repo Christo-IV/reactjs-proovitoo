494d8b3e6891a76817019de8b6a885719b05bf54
Christo-IV <christo.vahtramae@voco.ee>
2022-12-05 08:40:21 +0200

    Restricted nav content width to container

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,28 +15,30 @@ const Navbar = () => {
   return (
     <header>
       <nav className="navbar flex">
-        <button className="hamburger-toggle flex" onClick={toggleDropdown}>
-          <div className="hamburger-lines flex">
-            <span className="hamburger-line"></span>
-            <span className="hamburger-line"></span>
-            <span className="hamburger-line"></span>
-          </div>
-          <p className="hamburger-text">Menu</p>
-        </button>
-        <ul className="nav-links" style={{ display: display }}>
-          <li className="nav-link">
-            <a href="#">Home</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Blog</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Events</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Gallery</a>
-          </li>
-        </ul>
+        <div className="nav-content">
+          <button className="hamburger-toggle flex" onClick={toggleDropdown}>
+            <div className="hamburger-lines flex">
+              <span className="hamburger-line"></span>
+              <span className="hamburger-line"></span>
+              <span className="hamburger-line"></span>
+            </div>
+            <p className="hamburger-text">Menu</p>
+          </button>
+          <ul className="nav-links" style={{ display: display }}>
+            <li className="nav-link">
+              <a href="#">Home</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Blog</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Events</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Gallery</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
