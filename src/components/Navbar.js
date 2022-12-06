e0e81b58d2a538fd6641c7ea0815ce58bdd830ee
Christo-IV <65234973+Christo-IV@users.noreply.github.com>
2022-12-06 09:10:26 +0200

    Header alignment (#8)
    * Restricted nav content width to container
    
    * Various rem tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,33 +12,35 @@ const Navbar = () => {
   return (
     <header>
       <nav className="navbar flex">
-        <button
-          className={classNames("hamburger-toggle flex", {
-            "is-open": isNavMenuOpen,
-          })}
-          onClick={toggleDropdown}
-        >
-          <div className="hamburger-lines flex">
-            <span className="hamburger-line"></span>
-            <span className="hamburger-line"></span>
-            <span className="hamburger-line"></span>
-          </div>
-          <p className="hamburger-text">Menu</p>
-        </button>
-        <ul className="nav-links">
-          <li className="nav-link">
-            <a href="#">Home</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Blog</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Events</a>
-          </li>
-          <li className="nav-link">
-            <a href="#">Gallery</a>
-          </li>
-        </ul>
+        <div className="nav-content">
+          <button
+            className={classNames("hamburger-toggle flex", {
+              "is-open": isNavMenuOpen,
+            })}
+            onClick={toggleDropdown}
+          >
+            <div className="hamburger-lines flex">
+              <span className="hamburger-line"></span>
+              <span className="hamburger-line"></span>
+              <span className="hamburger-line"></span>
+            </div>
+            <p className="hamburger-text">Menu</p>
+          </button>
+          <ul className="nav-links">
+            <li className="nav-link">
+              <a href="#">Home</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Blog</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Events</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Gallery</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
