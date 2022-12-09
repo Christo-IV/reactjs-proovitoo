c7efb56f137dba3198c23aff550bde7325294d50
Christo-IV <christo.vahtramae@voco.ee>
2022-12-09 12:24:54 +0200

    Remove unused "heading" CSS class

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.scss";
 export const Header = ({ title }) => {
   return (
     <div className={styles["header"]}>
-      <h2 className={styles["heading"]}>{title}</h2>
+      <h2>{title}</h2>
       <span className={styles["divider"]}></span>
     </div>
   );
